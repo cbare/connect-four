ea8f84def7a8033b885c0f3ff8f1355239d1ff16
J. Christopher Bare <cbare@arivale.com>
2019-06-06 15:31:16 -0700

    ensure unique tokens

--- src/store.py
+++ src/store.py
@@ -12,6 +12,27 @@ from game import Game, Player
 
 GAMES = {}
 PLAYERS = {}
+ASSIGNED_TOKENS = set()
+POSSIBLE_TOKENS = 'abcdefghijklmnopqrstuvwxyz!@#$%^&*-+='
+
+
+def _assign_unique_token(proposed_token=None):
+    """
+    A hokey way of giving out unique tokens.
+    """
+    ## use the proposed token if not already assigned
+    if proposed_token and proposed_token not in ASSIGNED_TOKENS:
+        ASSIGNED_TOKENS.add(proposed_token)
+        return proposed_token
+
+    ## assign a symbol from back of list
+    for t in reversed(POSSIBLE_TOKENS):
+        if t not in ASSIGNED_TOKENS:
+            ASSIGNED_TOKENS.add(t)
+            return t
+
+    ## fresh out of tokens
+    raise RuntimeError('All tokens used.')
 
 
 def get_game(game_id):
@@ -33,7 +54,7 @@ def new_game(player_ids, rows, columns):
 
 def get_or_create_player(player_id):
     if player_id not in PLAYERS:
-        p = Player(player_id)
+        p = Player(player_id, token=_assign_unique_token(player_id[0] if player_id else None))
         PLAYERS[player_id] = p
     return PLAYERS[player_id]
 
